bbe8d75e4fc25cfbbec19a327a8b7e7a0a427183
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2022-08-31 21:33:35 -0700

    still trying

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 
 let lineNo = 1;
+let rowNumber = 0;
 
 
 		$(document).ready(function () {
 			$("#addRow").click(() => {
 
-				tableBody = $("table tbody");
+				let table = $("table tbody");
 				var nameInput = $('#firstName').val();
 				var lastName = $('#lastName').val();
 				var position = $('#inputGroupSelect01').val();
-				tableBody.append(`<tr id="newRow"><td> Player no.  ${lineNo}  </td>'  <td>  ${nameInput}  </td>
-				<td> ${lastName} </td> <td> ${position} </td> <td> <button type="button" id="edit" class="btn btn-warning">Edit Position</button> <button type="button"  class="btn btn-danger">Delete</button></td></tr>`);
-
+				let markUp =  (`<tr id="newRow"><td> Player no.  ${lineNo}  </td>'  <td>  ${nameInput}  </td>
+				<td> ${lastName} </td> <td> ${position} </td> `);
+				table.append(markUp)
+				$('newRow').attr(rowNumber)				
+				rowNumber++;
 				lineNo++;
+				editBtn(rowNumber)
 				console.log(nameInput);
+				console.log(rowNumber);
 
-
-				$('#edit').click(function () {
-					$('#newRow')
-
-
-					$('#newRow td:nth-child(4)').replaceWith(`<div class="input-group mb-3">
-                        <label class="input-group-text" for="inputGroupSelect01">Penis</label>
-                        <select class="form-select" id="inputGroupSelect01">
-                          <option selected id="position">Choose...</option>
-                          <option value="Point Gaurd">Point Gaurd</option>
-                          <option value="Shooting Gaurd">Shooting Gaurd</option>
-                          <option value="Small Forward">Small Forward</option>
-                          <option value="Power Forward">Power Forward</option>
-                          <option value="Center">Center</option>
+				
+				// $('#edit').click(function  () {
+					
+				// 	$('#newRow td:nth-child(4)').replaceWith(`<div class="input-group mb-3">
+                //         <label class="input-group-text" for="inputGroupSelect01">New Pos.</label>
+                //         <select class="form-select" id="inputGroupSelect01">
+                //           <option selected id="position">Choose...</option>
+                //           <option value="Point Gaurd">Point Gaurd</option>
+                //           <option value="Shooting Gaurd">Shooting Gaurd</option>
+                //           <option value="Small Forward">Small Forward</option>
+                //           <option value="Power Forward">Power Forward</option>
+                //           <option value="Center">Center</option>
                           
-                        </select>
-                      </div>`);
-				}
-				);
+                //         </select>
+                //       </div>`);
+				// }
+				// );
 
 
 
@@ -41,6 +44,30 @@ let lineNo = 1;
 				
                 
 			});
+
+function editBtn() {
+	let editButton = `<td> <button type="button" id="edit" class="btn btn-warning">Edit Position</button> <button type="button"  class="btn btn-danger">Delete</button></td> `
+	$('#newRow').append(editButton)
+	
+
+	
+	$('#edit').click = () => {
+		$('#newRow td:nth-child(4)').replaceWith(`<div class="input-group mb-3">
+                //         <label class="input-group-text" for="inputGroupSelect01">New Pos.</label>
+                //         <select class="form-select" id="inputGroupSelect01">
+                //           <option selected id="position">Choose...</option>
+                //           <option value="Point Gaurd">Point Gaurd</option>
+                //           <option value="Shooting Gaurd">Shooting Gaurd</option>
+                //           <option value="Small Forward">Small Forward</option>
+                //           <option value="Power Forward">Power Forward</option>
+                //           <option value="Center">Center</option>
+                          
+                //         </select>
+                //       </div>`);
+				
+				
+	}
+}
 		
 	
 			
