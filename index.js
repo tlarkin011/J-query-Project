4acabcc06b885ff8054c19289879ce1937c76f46
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2022-08-31 23:48:00 -0700

    Updated

--- index.js
+++ index.js
@@ -10,33 +10,40 @@ let rowNumber = 0;
 				var nameInput = $('#firstName').val();
 				var lastName = $('#lastName').val();
 				var position = $('#inputGroupSelect01').val();
-				let markUp =  (`<tr id="newRow"><td> Player no.  ${lineNo}  </td>'  <td>  ${nameInput}  </td>
-				<td> ${lastName} </td> <td> ${position} </td> `);
+				let markUp =  (`<tr value= ${rowNumber} id=newRow><td> Player no.  ${lineNo}  </td>'  <td>  ${nameInput}  </td>
+				<td> ${lastName} </td> <td> ${position} </td> <td> <button type="button" id="edit" class="btn btn-warning">Edit Position</button> <button type="button" id="delete"  class="btn btn-danger">Delete</button></td></tr>  `);
 				table.append(markUp)
-				$('newRow').attr(rowNumber)				
+				
+								
 				rowNumber++;
 				lineNo++;
-				editBtn(rowNumber)
+				
 				console.log(nameInput);
 				console.log(rowNumber);
+				console.log(markUp);
 
 				
-				// $('#edit').click(function  () {
+				$('#edit').click(function  () {
 					
-				// 	$('#newRow td:nth-child(4)').replaceWith(`<div class="input-group mb-3">
-                //         <label class="input-group-text" for="inputGroupSelect01">New Pos.</label>
-                //         <select class="form-select" id="inputGroupSelect01">
-                //           <option selected id="position">Choose...</option>
-                //           <option value="Point Gaurd">Point Gaurd</option>
-                //           <option value="Shooting Gaurd">Shooting Gaurd</option>
-                //           <option value="Small Forward">Small Forward</option>
-                //           <option value="Power Forward">Power Forward</option>
-                //           <option value="Center">Center</option>
+					$('#newRow td:nth-child(4)').replaceWith(`<div class="input-group mb-3">
+                        <label class="input-group-text" for="inputGroupSelect01">New Pos.</label>
+                        <select class="form-select" id="inputGroupSelect02">
+                          <option selected id="position">Choose...</option>
+                          <option value="Point Gaurd">Point Gaurd</option>
+                          <option value="Shooting Gaurd">Shooting Gaurd</option>
+                          <option value="Small Forward">Small Forward</option>
+                          <option value="Power Forward">Power Forward</option>
+                          <option value="Center">Center</option>
                           
-                //         </select>
-                //       </div>`);
-				// }
-				// );
+                        </select>
+                      </div>`);
+				}
+				);
+
+				$('#delete').click(function ()  {
+					$('#newRow').remove()
+
+				})
 
 
 
@@ -45,29 +52,10 @@ let rowNumber = 0;
                 
 			});
 
-function editBtn() {
-	let editButton = `<td> <button type="button" id="edit" class="btn btn-warning">Edit Position</button> <button type="button"  class="btn btn-danger">Delete</button></td> `
-	$('#newRow').append(editButton)
+
+				
 	
 
-	
-	$('#edit').click = () => {
-		$('#newRow td:nth-child(4)').replaceWith(`<div class="input-group mb-3">
-                //         <label class="input-group-text" for="inputGroupSelect01">New Pos.</label>
-                //         <select class="form-select" id="inputGroupSelect01">
-                //           <option selected id="position">Choose...</option>
-                //           <option value="Point Gaurd">Point Gaurd</option>
-                //           <option value="Shooting Gaurd">Shooting Gaurd</option>
-                //           <option value="Small Forward">Small Forward</option>
-                //           <option value="Power Forward">Power Forward</option>
-                //           <option value="Center">Center</option>
-                          
-                //         </select>
-                //       </div>`);
-				
-				
-	}
-}
 		
 	
 			
